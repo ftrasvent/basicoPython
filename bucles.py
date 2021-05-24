49302bf35562a918eaa1f8e8d50eebecac767907
Fernando Trasvent <ftrasvent@icloud.com>
2021-05-23 23:25:32 -0500

    ciclo while, ejercicio

--- bucles.py
+++ bucles.py
@@ -1,2 +1,16 @@
-contandor = 0
-print('2 elevado a ' + str(contandor) + ' es igual a: ' + str(2**(contandor)))
+# contandor = 0
+# print('2 elevado a ' + str(contandor) + ' es igual a: ' + str(2**(contandor))) 
+
+
+def run ():
+    LIMITE = 100000
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2)) 
+        contador = contador + 1
+        potencia_2 = 2**contador
+
+
+if __name__ == '__main__':
+    run()
