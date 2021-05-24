844668c5160de34733599678e5e1b90ab510ebd0
Fernando Trasvent <ftrasvent@icloud.com>
2021-05-24 00:07:49 -0500

    Ciclo for

---
+++ for.py
@@ -0,0 +1,16 @@
+# contador = 1
+# print(contador)
+# while contador < 100:
+#     contador += 1
+#     print(contador)
+
+# for contador in range(1, 21):
+#     print(contador)
+
+# for i in range(10):
+#     print(11 * i)
+
+for i in range(10):
+    print(f'Esta es la tabla del {i}')
+    for j in range (10):
+            print(f'{i} x {j} = {i*j}')
