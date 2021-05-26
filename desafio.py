7eceaaa1642b97576a6e7be312be9028946d4e82
Fernando Trasvent <ftrasvent@icloud.com>
2021-05-25 20:40:01 -0500

    Desafio de bucles, break

---
+++ desafio.py
@@ -0,0 +1,20 @@
+def run():
+    print('Bohemian Rhapsody''\n\n')
+    for i in range(0,3):
+        if i == 0:
+            print('Oh')
+        if i == 2:
+            print('Mama mia, let me go''\n')
+            break
+        print('Mama mia')
+
+    print('Beelzebub has a devil put aside')
+    for i in range(0,3):
+        if i == 2:
+            print('For meeeeeeeeee!')
+            break
+        print('For meee,')
+
+
+if __name__ == '__main__':
+    run()
